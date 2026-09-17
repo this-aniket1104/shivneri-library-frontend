d89e8551bb3c469178f2abc5243c9f4f6a9612ed
this-aniket1104 <aniket1104.in@gmail.com>
2026-09-17 14:35:02 +0530

    updated config

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  serverRuntimeConfig: {
-    API_URL: process.env.API_URL || "https://this-aniket1129-library-backend.hf.space"
-  },
   env: {
-    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || "https://this-aniket1129-library-backend.hf.space"
+    NEXT_PUBLIC_API_URL: "https://this-aniket1129-library-backend.hf.space"
   }
 };
 
